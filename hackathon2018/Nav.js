9feebfd77b51faaeb580e416b2f5e30a97c89cb5
Neil Truscott <ntruscott@asicentral.com>
2018-01-23 13:21:24 -0500

    wip(Dashboard)

--- hackathon2018/Nav.js
+++ hackathon2018/Nav.js
@@ -35,4 +35,44 @@ function switchMode() {
   for (var i = 0; i <= Math.min(contentList.length, containerList.length); i++) {
     $('#' + containerList[i]).html($('#' + contentList[i] + '-container').html());
   }
+  renderEspWebChart();
+}
+
+function renderEspWebChart(){
+  var chartDiv = "espWebContentPieChart";
+  var chart = new CanvasJS.Chart(chartDiv,
+  {
+      height: 225,
+      width:300,
+      animationEnabled: true,
+      animationDuration: 500,
+      theme: "theme2",
+      title:{
+          text: "Top Searches",
+          fontSize:18
+      },		
+      data: [
+      {       
+          type: "pie",
+          showInLegend: false,
+          toolTipContent: "{indexLabel} - #percent %",
+          yValueFormatString: "#,##0,,.##",
+          legendText: "{indexLabel}",
+          dataPoints: [
+              {  y: 30, indexLabel: "Apparel" },
+              {  y: 26, indexLabel: "Pens" },
+              {  y: 24, indexLabel: "Bags" },
+              {  y: 21, indexLabel: "Lanyards"},
+              {  y: 13, indexLabel: "Mugs" },
+              {  y: 8, indexLabel: "Water Bottles"},
+              {  y: 5, indexLabel: "Headwear"},
+              {  y: 3, indexLabel: "Patches"},
+              {  y: 2, indexLabel: "Banners"},
+              {  y: 1, indexLabel: "Sunglasses"}
+              
+          ]
+      }
+      ]
+  });
+chart.render();
 }
