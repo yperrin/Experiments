92c33fdce9cd0e91c9fa0d0f82edfd8d13a6f072
yperrin <perrin.yann@gmail.com>
2019-05-24 15:12:11 -0400

    adding partial update

--- Projects/ng-template/src/app/play/services/play.service.ts
+++ Projects/ng-template/src/app/play/services/play.service.ts
@@ -1,34 +1,46 @@
 import { Subject, Observable, of, forkJoin } from 'rxjs';
 import { PlayViewModel, PlayFormViewModel } from '../models/playViewModel.model';
-import { tap, map, delay } from 'rxjs/operators';
+import { tap, map, delay, shareReplay } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+
+const form$ = of(new PlayFormViewModel({first: 'First', second: 'Second'})).pipe(
+    delay(1000),
+    tap(() => console.log('service getting Form'))
+);
 
 @Injectable({
     providedIn: 'root'
 })
 export class PlayService {
     private vmSubject = new Subject<PlayViewModel>();
+    private currentVM: PlayViewModel;
     vm$ = this.vmSubject.asObservable();
 
     init$(): Observable<PlayViewModel> {
         const playGeneral = new PlayViewModel({option: true});
-        const playForm = new PlayFormViewModel({first: 'First', second: 'Second'});
 
         const general$ = of(playGeneral).pipe(
             delay(1000),
-            tap(() => console.log('service getting general'))
-        );
-        const form$ = of(playForm).pipe(
-            delay(1000),
-            tap(() => console.log('service getting Form'))
+            tap(() => console.log('service getting general')),
+            shareReplay()
         );
         return forkJoin(general$, form$).pipe(
             map( value => {
                 const vm = new PlayViewModel({form: value[1], option: value[0].option});
                 console.log('Getting value from fork Join');
+                this.currentVM = vm;
                 this.vmSubject.next(vm);
                 return vm;
             })
         );
+    }
+
+    save$() {
+        return form$.pipe(
+            tap(value => {
+                this.currentVM.form = value;
+                this.vmSubject.next(this.currentVM);
+            })
+        )
     }
 }
